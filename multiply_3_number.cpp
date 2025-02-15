158741816b2109b338bb8d6510285b8fb1d96fc7
abishek <abisheklekhak07@gmail.com>
2025-02-15 22:39:37 +0530

    multiply_3_number.cpp

---
+++ multiply_3_number.cpp
@@ -0,0 +1,15 @@
+//wap to multiply 3 no's take input from the user.
+#include<iostream>
+using namespace std;
+
+    int main() {
+
+      int a,b,c;
+
+      cout<<"Enter any 3 number : ";
+      cin>>a>>b>>c;
+
+      cout<<"multiple is : "<<a*b*c;
+
+      return 0;
+    }
